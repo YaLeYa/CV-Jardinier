0e3b501dac310d71879d84874cef189cfbefb898
HakaiTrader <musicus@live.be>
2026-03-11 16:19:46 +0100

    Mise à jour majeur

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-const attestation = document.querySelector('.attestation');
-
-let toggle = false;
-console.log(window);
-const scaling = () => {
-    if (toggle === false) {
-        attestation.style.transform = 'scale(20)';
-        toggle = true;
-        if (window.innerWidth < 900) {
-            attestation.style.transform = 'scale(10)';
-            toggle = true;
-        }
-
-    } else if (toggle = true) {
-        attestation.style.transform = 'scale(1)'
-        toggle = false;
-    }
-
-}
-attestation.addEventListener('click', scaling);
-
 // SLIDER //
-
-
 const slide = document.querySelectorAll('.slides')
 const next = document.querySelector('.next')
 const previous = document.querySelector('.prev')
